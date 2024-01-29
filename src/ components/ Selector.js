f48c085f0fecba7207a8cbbadbc13678e540a2d3
mkato-manaby <mkato.manaby@gmail.com>
2024-01-29 20:13:54 +0900

    p191まで終了

--- src/ components/ Selector.js
+++ src/ components/ Selector.js
@@ -1,18 +1,19 @@
 // Selector.js
 
-import countriesJson from "../countries.json";
-
-const Selector = () => {
-    console.log(countriesJson)
+const Selector = (props) => {
     return (
         <div>
-            <select>
-                <option>United States</option>
-                <option>China</option>
-                <option>Japan</option>
+            <select onChange={(e) => props.setCountry(e.target.value)}>
+                <option>Select A Country</option>
+                {props.countriesJson.map((country, index) =>
+                    <option key={index} value={country.Slug}>{country.Country}</option>
+                )}
             </select>
+            <button onClick = {props.getCountryData}>Get Data</button>
         </div>
     );
 };
+
+
 
 export default Selector;
